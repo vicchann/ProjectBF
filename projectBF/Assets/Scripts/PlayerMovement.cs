39b5a8750e09cd219a358d1e8a0fc540a37a0fe9
fadedakuma <xfadedakumax@gmail.com>
2020-02-14 17:15:44 -0800

    Player Movement w/ Jump

--- projectBF/Assets/Scripts/PlayerMovement.cs
+++ projectBF/Assets/Scripts/PlayerMovement.cs
@@ -4,35 +4,49 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
+    private float moveInput;
     public float moveSpeed;
-    private Vector2 direction;
-    // private Animator animator;
+    public float jumpForce;
 
-    // void start(){animator = GetComponent<Animator>();}
+    private Rigidbody2D rb;
+
+    private bool isGrounded;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+
+    private int extraJumps;
+    public int extraJumpsValue;
+
+
+    void Start()
+    {
+        extraJumps = extraJumpsValue;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+
+        moveInput = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+    }
 
     void Update()
     {
-        TakeInput();
-        Move();
-    }
-
-	private void Move() {
-        transform.Translate(direction * moveSpeed * Time.deltaTime);
-	}
-
-    private void TakeInput() {
-
-        direction = Vector2.zero;
-
-        if (Input.GetKey(KeyCode.A))
+        if (isGrounded == true)
         {
-            direction += Vector2.left;
-            // animator.Play("playerRight");
+            extraJumps = extraJumpsValue;
         }
 
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKeyDown(KeyCode.W) && extraJumps > 0)
         {
-            direction += Vector2.right;
+            rb.velocity = Vector2.up * jumpForce;
+            extraJumps--;
+        } else if (Input.GetKeyDown(KeyCode.W) && extraJumps == 0 && isGrounded == true)
+        {
+            rb.velocity = Vector2.up * jumpForce;
         }
     }
 }
