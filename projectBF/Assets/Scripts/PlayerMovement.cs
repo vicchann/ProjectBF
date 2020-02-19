93a853807f3eaadca938a9ad65bad290bd02dc53
fadedakuma <xfadedakumax@gmail.com>
2020-02-18 16:42:21 -0800

    Player Movement With Sprint

--- projectBF/Assets/Scripts/PlayerMovement.cs
+++ projectBF/Assets/Scripts/PlayerMovement.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour
+{
 
     private float moveInput;
+    public float sprintSpeed;
+    private float sprint;
     public float moveSpeed;
     public float jumpForce;
+
 
     private Rigidbody2D rb;
 
@@ -29,8 +33,22 @@ public class PlayerMovement : MonoBehaviour {
     {
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
 
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            sprint = sprintSpeed;
+        }
+        else
+        {
+            sprint = 1;
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift) == false)
+        {
+            sprint = 1;
+        }
+
         moveInput = Input.GetAxisRaw("Horizontal");
-        rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+        rb.velocity = new Vector2(moveInput * moveSpeed * sprint, rb.velocity.y);
     }
 
     void Update()
@@ -44,7 +62,8 @@ public class PlayerMovement : MonoBehaviour {
         {
             rb.velocity = Vector2.up * jumpForce;
             extraJumps--;
-        } else if (Input.GetKeyDown(KeyCode.W) && extraJumps == 0 && isGrounded == true)
+        }
+        else if (Input.GetKeyDown(KeyCode.W) && extraJumps == 0 && isGrounded == true)
         {
             rb.velocity = Vector2.up * jumpForce;
         }
