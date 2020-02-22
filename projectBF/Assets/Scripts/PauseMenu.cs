cc1bf56e36cb036921eefd1f7c7fa7ef979b8670
vicchann <60677482+vicchann@users.noreply.github.com>
2020-02-21 17:06:58 -0800

    no message

---
+++ projectBF/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{ 
+    public void ResumeGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    public void Options()
+    {
+        Debug.Log("1");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit(); 
+    }
+}
